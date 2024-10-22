1d3c43f111732d40b0860212d076bc39a234f9af
F-Chitz <f.chitz@outlook.com>
2024-10-22 01:04:42 -0300

    Shell Sort

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
 const $container = document.getElementById("container");
 let arrAnimation = [];
 let run = undefined;
-let iAnimation = 0;
 
 
+
+function shellSort(array){
+let l = array.length;
+arrAnimation = [];
+for (let gap = Math.floor(l/2); gap > 0 ; gap = Math.floor(gap/2)) {
+    for (let i = gap; i < l; i++){
+
+        let swap = array[i];
+        let j;
+        for(j = i; j>= gap && Number(array[j-gap].style.height.replace("%","")) > Number(swap.style.height.replace("%","")); j -= gap){
+            array[j] = array[j-gap];
+        }
+        array[j] = swap;
+    }
+    arrAnimation.push([...array]);    
+}
+}
 
 function BubbleSort(array) {
     arrAnimation = []
@@ -36,13 +52,18 @@ function insertionSort(array) {
     }
 
 }
+let iAnimation = 0;
 
 function animation() {
+
+    console.log("start");
     if (iAnimation < arrAnimation.length) {
         $container.innerHTML = "";
         $container.append(...arrAnimation[iAnimation++]);
     } else {
         clearInterval(run);
+    console.log("end");
+    iAnimation = 0;
         run = undefined;
 
     }
@@ -51,13 +72,15 @@ function animation() {
 document.getElementById("setup").addEventListener("click", () => {
     $container.innerHTML = "";
     clearInterval(run);
+    let size = document.getElementById("size").value;
 
 
-    for (let i = 0; i < 30; i++) {
+    for (let i = 0; i < size; i++) {
         const rect = document.createElement("div");
         rect.classList.add("rectangle");
         let rdn = parseInt(Math.random() * 100) + 1;
         rect.style.height = rdn + "%";
+        rect.style.width = Math.floor($container.clientWidth/size) + "px";
 
         $container.appendChild(rect);
 
@@ -76,5 +99,11 @@ document.getElementById("bubbleSort").addEventListener("click", () => {
 
     BubbleSort([...$container.children]);
     run = setInterval(animation, 60);
+
+});
+document.getElementById("shellSort").addEventListener("click", () => {
+
+    shellSort([...$container.children]);
+    run = setInterval(animation, document.getElementById("speed").value);
 
 });
